dce78f4476bd2a333d598bd055ff81d929d84bc5
Anton Bagryanov <abagraynov@cindicator.com>
2021-08-18 02:23:06 +0300

    [Style] Remove unused imports. Downgrade aioredis.

--- chatbot/app.py
+++ chatbot/app.py
@@ -6,13 +6,7 @@ from aiogram import executor
 from loguru import logger
 
 from handlers.connect import dp
-# from loader import bot
-# from utils.func import to_json
 from load.launch_bot import on_startup, on_shutdown
-# from load.sender_consumer import consume_new_messages
-# from utils.async_func import async_task
-# from handlers.errors.send_exception import send_exception
-
 from settings import settings
 
 
@@ -24,7 +18,6 @@ def start_bot() -> None:
     logger.info(
         f'Running in {"DEBUG" if settings.DEBUG else "PRODUCTION"} mode'
     )
-    # async_task(consume_new_messages())
     executor.start_polling(
         dp,
         on_startup=on_startup,
--- chatbot/loader.py
+++ chatbot/loader.py
@@ -1,12 +1,8 @@
 """
 Module where so-called reusable global objects are initialized
 """
-import asyncio
-import logging
 import aiogram
-# from aiogram.contrib.fsm_storage.memory import MemoryStorage
-# from aiogram.contrib.fsm_storage.redis import RedisStorage2
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage import memory, redis
 from loguru import logger
 
 from settings import settings
@@ -19,13 +15,13 @@ bot = aiogram.Bot(
     token=settings.BOT_TOKEN,
     parse_mode=aiogram.types.ParseMode.HTML
 )
-# if settings.REDIS_HOST == '':
-if True:
-    storage = MemoryStorage()
+if settings.REDIS_HOST == '':
+    storage = memory.MemoryStorage()
 else:
-    storage = RedisStorage2(
+    storage = redis.RedisStorage2(
             host=settings.REDIS_HOST,
             port=settings.REDIS_PORT,
+            prefix='chatbot',
     )
 
 dp = patch_dispatcher(
